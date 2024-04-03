3686c63b79ccd406da98287d1f3c9a70a3094184
Shana <shana.hartwick@gmail.com>
2024-04-03 17:33:59 -0400

    creating base class

---
+++ Sensor.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef SENSOR_H
+#define SENSOR_H
+
+class Sensor
+{
+public:
+    // Virtual function for simulating data collection
+    virtual void gatherData() = 0;
+
+    // Virtual function for data processing
+    virtual void processData() = 0;
+
+    // Virtual destructor
+    virtual ~Sensor() {}
+};
+
+#endif // SENSOR_H
