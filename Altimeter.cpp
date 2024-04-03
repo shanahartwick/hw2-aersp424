9b175b1bc6a5c8d4733867bf3ab01166e1094037
Shana <shana.hartwick@gmail.com>
2024-04-03 17:37:28 -0400

    creating sensor derived class

---
+++ Altimeter.cpp
@@ -0,0 +1,16 @@
+#include "Altimeter.h"
+#include <iostream>
+
+// Simulate data collection for altimeter
+void Altimeter::gatherData() {
+    // Simulate altimeter data collection
+    std::cout << "Gathering data from Altimeter Sensor." << std::endl;
+    // Code to collect altimeter data
+}
+
+// Process altimeter data
+void Altimeter::processData() {
+    // Simulate processing altimeter data
+    std::cout << "Processing data from Altimeter Sensor." << std::endl;
+    // Code to process altimeter data
+}
---
+++ Altimeter.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef ALTIMETER_H
+#define ALTIMETER_H
+#include "Sensor.h"
+
+class Altimeter :
+    public Sensor
+{
+public:
+    // Simulate data collection for altimeter
+    void gatherData() override;
+
+    // Process altimeter data
+    void processData() override;
+};
+
+#endif // ALTIMETER_H
