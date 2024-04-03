f77d280732a8693f6545bb5490153d3438547202
Shana <shana.hartwick@gmail.com>
2024-04-03 18:51:09 -0400

    changing some class definitions from public to private

--- RobotTaskSimulator.h
+++ RobotTaskSimulator.h
@@ -12,22 +12,19 @@
 class RobotTaskSimulator
 {
 public:
-    // Function to represent the behavior of each robot
-    void robotTask(int robotID);
-
     // Function to run the simulation
     void runSimulation();
 
 private:
-    // Mutex for synchronizing access to output stream
-    std::mutex coutMutex;
-
     // Define the number of robots and tools
     static const int numRobots = 5;
     static const int numTools = 5;
 
     // Mutexes for synchronizing access to tools
     std::mutex toolMutex[numTools];
+
+    // Function to represent the behavior of each robot
+    void robotTask(int robotID);
 };
 
 #endif // ROBOT_TASK_SIMULATOR_H
