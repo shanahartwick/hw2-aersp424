342268859fc773f4bf14e94aa71abe0b447547bb
Shana <shana.hartwick@gmail.com>
2024-04-03 17:39:25 -0400

    creating airspeed sensor derived class

---
+++ AirspeedSensor.cpp
@@ -0,0 +1,16 @@
+#include "AirspeedSensor.h"
+#include <iostream>
+
+// Simulate data collection for airspeed
+void AirspeedSensor::gatherData() {
+    // Simulate airspeed data collection
+    std::cout << "Gathering data from Airspeed Sensor." << std::endl;
+    // Code to collect airspeed data
+}
+
+// Process airspeed data
+void AirspeedSensor::processData() {
+    // Simulate processing airspeed data
+    std::cout << "Processing data from Airspeed Sensor." << std::endl;
+    // Code to process airspeed data
+}
---
+++ AirspeedSensor.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef AIRSPEED_SENSOR_H
+#define AIRSPEED_SENSOR_H
+#include "Sensor.h"
+
+class AirspeedSensor :
+    public Sensor
+{
+    // Simulate data collection for airspeed
+    void gatherData() override;
+
+    // Process airspeed data
+    void processData() override;
+};
+
+#endif // AIRSPEED_SENSOR_H
